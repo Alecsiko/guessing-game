de8cab19cb2dedb6f3540459c18e0a758a1537e4
Aleksey <104904174+Alecsiko@users.noreply.github.com>
2022-08-16 20:09:00 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+       this.min = min;
+       this.max = max;
 
     }
 
     guess() {
 
+        let thisNum = Math.round((this.min + this.max)/2)
+        return thisNum
+
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
